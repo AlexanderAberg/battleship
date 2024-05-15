e36510450606bf7661872c8bc2fefe09dd277075
Alexander Åberg <alexander_aberg1985@msn.com>
2024-05-15 09:20:04 +0000

    Put in some information from Love Maths to adjust later and also started a making a function to get ships on board

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,21 @@
 //Game should not start until the DOM is finished loading
 document.addEventListener('DOMContentLoaded');
+
+let buttons = document.getElementsByTagName('button');
+
+for (let button of buttons) {
+    button.addEventListener('click, function(') {
+        if (this.getAttribute('data-type') === 'submit') {
+            alert('You clicked Submit!');
+        } else {
+            let fameType = this.getAttribute('data-type');
+            runGame();
+        }
+    }
+}
+
+function () {
+    let location;
+    let randomStartIndex = Math.floor(Math.random() * width * width)
+    document.getElementsByClassName('ships-container')
+}

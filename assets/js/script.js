eee24d8392c2425ed9cfa8b08a403fe171cfa5ec
Alexander Aberg <alexander_aberg1985@msn.com>
2024-05-17 22:01:55 +0000

    Added a couple of more semicolon

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ for (let square of squares) {
         //       let gameType = this.getAttribute('data-type');
         //       runGame();
         //   }
-    })
+    });
 }
 
 //Placement of computer ships
@@ -103,7 +103,7 @@ function handleClick(e) {
 
 function checkShip(ship, shipLength) {
     if (user === 'player') {
-        infoDisplay.textContent = `You sank the enemies ship ${ship}`
+        infoDisplay.textContent = `You sank the enemies ship ${ship}`;
         playerHits = userHits.filter(ship => ship !== ship);
     }
 }
